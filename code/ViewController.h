218323711ac229f8572f22767ded922a98e106dc
WiitterSimithYU <378961614@qq.com>
2018-06-19 16:05:15 +0800

    commit

---
+++ code/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  code
+//
+//  Created by miaomiaokeji on 2018/6/14.
+//  Copyright © 2018年 weiliams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ code/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  code
+//
+//  Created by miaomiaokeji on 2018/6/14.
+//  Copyright © 2018年 weiliams. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "BSYUI.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    BSYView *views = [[BSYView alloc] init];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
